1db255ebd49f289e20c0a5f21ca7ed0d254cc290
Volkova-D-O <Volkova.d.o@mail.ru>
2025-11-03 22:23:05 +0500

    some changes

--- src/main/java/ru/wteam/gateway_service/GatewayServiceApplication.java
+++ src/main/java/ru/wteam/gateway_service/GatewayServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 
 public class GatewayServiceApplication {
 
